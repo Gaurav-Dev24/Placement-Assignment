ac23bdbc0f6cbf97b2b176b2ad0fa7f0628026e6
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-06-18 12:59:27 +0530

    Counter app added

---
+++ React Answers/Counter App/counter/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const App = () => {
+
+  const[count, setCount] = useState(0);
+
+  return (
+    <div className='container my-5'>
+      <div className='card text-center my-5'>
+        <div className='card-body'>
+          <h1>Counter App</h1>
+          <div className='my-5'>
+            <h2 className='my-5'>{count}</h2>
+            <button className='btn btn-success mx-3' onClick={() => setCount(count + 1)}>Increment</button>
+            <button className='btn btn-danger mx-3' onClick={() => setCount(count - 1)} disabled={count === 0}>Decrement</button>
+            <button className='btn btn-secondary mx-3' onClick={() => setCount(0)} disabled={count === 0}>Reset</button>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
